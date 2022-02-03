33cf3f73a2044156d6bb17399ffd758cc074dc53
sll002 <sll002@ucsd.edu>
2022-02-03 14:13:57 -0800

    failing test commit push

---
+++ SkillDemoTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+import java.util.*;
+
+public class SkillDemoTest {
+    @Test
+    public void testOne(){
+        final String EXPECTED_STRING = "SamuelLiu";
+        //this is a failing test
+        assertEquals("failing test", EXPECTED_STRING, SkillDemo.stringConcatenator("Samuel", "Li"));
+    }
+}
